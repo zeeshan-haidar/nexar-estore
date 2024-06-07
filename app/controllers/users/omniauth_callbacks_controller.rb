e20bd8facae6874ea12f33949dc33a90b5366704
Zeeshan Haider <haider_csharp@hotmail.com>
2024-06-07 23:42:39 +0500

    rubocop linting applied

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
     def facebook
       @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
