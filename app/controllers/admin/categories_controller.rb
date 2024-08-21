4659902e50513de88b1cc5ab14a4dcf3e400c329
Zeeshan Haider <haider_csharp@hotmail.com>
2024-08-21 11:49:48 +0500

    updated cetagories managment views to mdb

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,11 +3,7 @@ class Admin::CategoriesController < ApplicationController
 
   # GET /admin/categories or /admin/categories.json
   def index
-    @categories = Category.all
-  end
-
-  # GET /admin/categories/1 or /admin/categories/1.json
-  def show
+    @categories = Category.all.order(id: :asc).page(params[:page]).per(10) # 10 products per page
   end
 
   # GET /admin/categories/new
@@ -25,11 +21,9 @@ class Admin::CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to admin_category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
+        format.html { redirect_to admin_categories_path, notice: "Category was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +32,9 @@ class Admin::CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to admin_category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
+        format.html { redirect_to admin_categories_path, notice: "Category was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +45,6 @@ class Admin::CategoriesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to admin_categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   namespace :admin do
-    resources :categories
+    resources :categories, except: [:show]
     resources :products, except: [:show]
   end
 
