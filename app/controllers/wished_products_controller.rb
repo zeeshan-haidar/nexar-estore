36e9106863a055b3f9aea74d5083829cc0390112
Zeeshan Haider <haider_csharp@hotmail.com>
2024-10-02 19:07:01 +0500

    wished products controller and main page added

--- app/controllers/wished_products_controller.rb
+++ app/controllers/wished_products_controller.rb
@@ -1,14 +1,17 @@
 class WishedProductsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @wished_products =  Product.joins(:wished_products).where(wished_products: {user_id: current_user.id}).distinct.order(id: :asc).page(params[:page]).per(8)
+  end
+
   def create
-    product = Product.find(params[:product_id])
-    wished_product = current_user.wished_products.build(product: product)
+    wished_product = current_user.wished_products.build(product_id: params[:product_id])
 
     if wished_product.save
-      redirect_to request.original_url, notice: "Added to your favourites."
+      redirect_to request.referer, notice: "Product added to your wishlist."
     else
-      redirect_to request.original_url, alert: "You have already favourited this product."
+      redirect_to request.referer, alert: "You already have this product in wishlist"
     end
   end
 
